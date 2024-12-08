36f291eaa63a88e4bbabc0b0d5bfa877a291676e
jorgerubin <42123961+jorgerubin@users.noreply.github.com>
2024-12-08 15:33:03 +0100

    fix: createProductReviews

--- routes/createProductReviews.ts
+++ routes/createProductReviews.ts
@@ -12,20 +12,16 @@ import { challenges } from '../data/datacache'
 
 const security = require('../lib/insecurity')
 
-module.exports = function productReviews () {
-  return (req: Request, res: Response) => {
-    const user = security.authenticatedUsers.from(req)
-    challengeUtils.solveIf(challenges.forgedReviewChallenge, () => { return user && user.data.email !== req.body.author })
-    reviewsCollection.insert({
-      product: req.params.id,
-      message: req.body.message,
-      author: req.body.author,
-      likesCount: 0,
-      likedBy: []
-    }).then(() => {
-      res.status(201).json({ status: 'success' })
-    }, (err: unknown) => {
-      res.status(500).json(utils.getErrorMessage(err))
-    })
-  }
+module.exports = () => (req: Request, res: Response) => {
+  const user = security.authenticatedUsers.from(req)
+  challengeUtils.solveIf(challenges.forgedReviewChallenge, () => user?.data.email !== req.body.author)
+
+  reviewsCollection.insert({
+    product: req.params.id,
+    message: req.body.message,
+    author: req.body.author,
+    likesCount: 0,
+    likedBy: []
+  }).then(() => res.status(201).json({ status: 'success' }))
+    .catch((err) => res.status(500).json(utils.getErrorMessage(err)))
 }
