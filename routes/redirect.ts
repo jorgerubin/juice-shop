63a85bb3a0be8717874068d77ea693e8486d2c6a
jorgerubin <42123961+jorgerubin@users.noreply.github.com>
2024-12-12 00:35:51 +0100

    fix: redirect

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -12,14 +12,6 @@ const security = require('../lib/insecurity')
 
 const allowedDomains = ['example.com', 'another-example.com'] // Dominios permitidos
 
-function isValidUrl(url) {
-  try {
-    const parsedUrl = new URL(url)
-    return allowedDomains.includes(parsedUrl.hostname)
-  } catch (err) {
-    return false
-  }
-}
 
 module.exports = function performRedirect () {
   return ({ query }: Request, res: Response, next: NextFunction) => {
@@ -27,14 +19,14 @@ module.exports = function performRedirect () {
     if (security.isRedirectAllowed(toUrl)) {
       challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => { return toUrl === 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' || toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' || toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6' })
       challengeUtils.solveIf(challenges.redirectChallenge, () => { return isUnintendedRedirect(toUrl) })
-      if (isValidUrl(toUrl)) {
-        res.redirect(toUrl)
+      if (allowedDomains.includes(toUrl)) {
+        res.redirect(toUrl);
       } else {
-        res.redirect('/')
+        res.status(400).send('Redirección no permitida');
       }
     } else {
-      res.status(406)
-      next(new Error('Unrecognized target URL for redirect: ' + toUrl))
+    res.status(406)
+    next(new Error('Unrecognized target URL for redirect: ' + toUrl))
     }
   }
 }
