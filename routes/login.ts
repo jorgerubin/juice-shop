56869fa2bf5034ef91082aee6df68d1ee6f4945d
jorgerubin <42123961+jorgerubin@users.noreply.github.com>
2024-12-11 02:11:39 +0100

    fix: login

--- routes/login.ts
+++ routes/login.ts
@@ -34,7 +34,17 @@ module.exports = function login () {
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
     sqlsentence = `SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }
-    models.sequelize.query(sqlsentence) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    
+    const replacements = {
+      email: req.body.email || '',
+      password: security.hash(req.body.password || ''),
+    };
+    
+    models.sequelize.query(sqlsentence,{
+      replacements,
+      model: UserModel,
+      plain: true,
+    }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
