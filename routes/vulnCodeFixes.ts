b23cc6fa34dc9c6f1c44d55abd37fc3521561eb9
jorge <jorgerubin271@gmail.com>
2024-12-12 18:23:18 +0100

    fix vulnCodeFixes

--- routes/vulnCodeFixes.ts
+++ routes/vulnCodeFixes.ts
@@ -82,7 +82,7 @@ export const checkCorrectFix = () => async (req: Request<Record<string, unknown>
     let safePath = baseDir + fileuser
 
     if (safePath.startsWith(baseDir)) {
-      const codingChallengeInfos = yaml.load(fs.readFileSync(safePath, 'utf8'))
+      const codingChallengeInfos = yaml.load(fs.readFileSync(baseDir + fileuser, 'utf8'))
       const selectedFixInfo = codingChallengeInfos?.fixes.find(({ id }: { id: number }) => id === selectedFix + 1)
       if (selectedFixInfo?.explanation) explanation = res.__(selectedFixInfo.explanation)
     
