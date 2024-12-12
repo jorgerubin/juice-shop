0257ee4a689ce6c4cfafd62e8224fa412124f1b2
jorgerubin <42123961+jorgerubin@users.noreply.github.com>
2024-12-12 02:27:48 +0100

    fix vulnCodeFixes

--- routes/vulnCodeFixes.ts
+++ routes/vulnCodeFixes.ts
@@ -79,7 +79,8 @@ export const checkCorrectFix = () => async (req: Request<Record<string, unknown>
     let explanation
     let pathinfo = './data/static/codefixes/'
     let file = `${key}.info.yml`
-    if (fs.existsSync(file, { root: pathinfo })) {
+    let existFile = fs.existsSync(file, { root: pathinfo })
+    if (existFile) {
       const codingChallengeInfos = yaml.load(fs.readFileSync(pathinfo, 'utf8'))
       const selectedFixInfo = codingChallengeInfos?.fixes.find(({ id }: { id: number }) => id === selectedFix + 1)
       if (selectedFixInfo?.explanation) explanation = res.__(selectedFixInfo.explanation)
